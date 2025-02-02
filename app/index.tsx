e2d7d7ae459e7a78b5fc3d25f1271aeba3acb2d4
JF195 <davidefuoco63@gmail.com>
2025-02-02 15:07:43 -0500

    add preview of scanning result

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Camera from '../components/camera';
+import { useState } from 'react';
+import { BarcodeScanningResult } from 'expo-camera';
 
 export default function App() {
+  const [scanValue, setScanValue] = useState<string>("Nothing scanned.");
   return (
     <View style={styles.container}>
       <Text>Scan QR Code to connect</Text>
-      <Camera barcodeType='qr' width={300} height={300}/>
+      <Camera barcodeType='qr' width={300} height={300} onBarcodeScanned={(value: BarcodeScanningResult)=>{
+        setScanValue(value.data)
+      }}/>
+      <Text>{scanValue}</Text>
       <StatusBar style="auto" />
     </View>
   );
