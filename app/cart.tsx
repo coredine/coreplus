98500a6ef94c813593d9a4ac5740d9679a65eef5
JF195 <davidefuoco63@gmail.com>
2025-02-11 11:26:00 -0500

    add cart page

---
+++ app/cart.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import ResizableComponent from '../components/ResizableComponent';
+
+
+export default function App() {
+  return (
+    <ResizableComponent childOne={ ( <Text>Camera</Text> ) } childTwo={ ( <Text>Shopping cart</Text> ) } />
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    width: '100%',
+    textAlign: "center",
+    margin: 2,
+    backgroundColor: ""
+  }
+});
