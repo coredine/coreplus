ec7275d638bcecd6bee34585a30d3decd428c4f8
JF195 <davidefuoco63@gmail.com>
2025-02-12 20:03:57 -0500

    Add scanner to the cart page

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,15 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
 import ResizableComponent from '../components/ResizableComponent';
+import Camera from '../components/camera';
 
 
 export default function App() {
   return (
-    <ResizableComponent childOne={ ( <Text>Camera</Text> ) } childTwo={ ( <Text>Shopping cart</Text> ) } />
+    <ResizableComponent childOne={ ( 
+      <View style={styles.container}>
+        <Text style={styles.text}>Scanner</Text> 
+        <Camera barcodeType='qr' width={300} height={180} />
+      </View>
+    ) } childTwo={ ( 
+      <Text>Shopping cart</Text> 
+    ) } />
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },text: {
     width: '100%',
     textAlign: "center",
     margin: 2,
