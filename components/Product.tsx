6f62395aa58532542ac98a4306c988a94f2b2b80
JF195 <davidefuoco63@gmail.com>
2025-02-16 12:54:42 -0500

    make product card swipeable

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,7 @@
 import { View, Text, Image } from "react-native";
+import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Reanimated from 'react-native-reanimated';
 
 export interface Product {
   sku: string;
@@ -14,13 +17,37 @@ interface ProductProps extends Product{
 
 export default function ProductCard(props: ProductProps){
   const noImage = require("../assets/no-image.png");
+
+  const RemoveItem = () => {
+
+    return (
+      <Reanimated.View style={{width:64}} className="bg-red-500 justify-center">
+        <Text className="text-center text-2xl text-white">X</Text>
+      </Reanimated.View>
+    )
+  }
+
   return (
-    <View className="flex flex-row w-100 justify-between bg-red-300 m-2 mx-6 p-2 rounded-lg">
-      <Image style={{width:50, height:50}} className="w-100 h-100" src={props.picture} source={noImage} />
-      <View className="align-middle">
-        <Text className="text-right text-xl font-extrabold"> {props.title} </Text>
-        <Text className="text-right"> {props.price} + TAX </Text>
-      </View>
-    </View>
+    <GestureHandlerRootView>
+      <ReanimatedSwipeable 
+        containerStyle={{
+          margin:6, marginLeft:12, marginRight:12,
+          borderRadius:8
+        }}
+        friction={2.5}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={45}
+        renderRightActions={RemoveItem}
+        >
+        <View className="flex flex-row w-100 justify-between bg-white p-2">
+          <Image style={{width:50, height:50}} className="w-100 h-100" src={props.picture} source={noImage} />
+          <View className="align-middle">
+            <Text className="text-right text-xl font-extrabold"> {props.title} </Text>
+            <Text className="text-right"> {props.price} + TAX </Text>
+          </View>
+        </View>
+      </ReanimatedSwipeable>
+      
+    </GestureHandlerRootView>
   )
 }
