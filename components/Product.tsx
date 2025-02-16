adb0275e7786971dc53da0867a7b4a90584049de
JF195 <davidefuoco63@gmail.com>
2025-02-16 12:28:26 -0500

    make product card

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,3 +1,4 @@
+import { View, Text, Image } from "react-native";
 
 export interface Product {
   sku: string;
@@ -6,4 +7,16 @@ export interface Product {
   title: string;
   picture?: string;
 }
-  
+
+export default function ProductCard(props: Product){
+  const noImage = require("../assets/no-image.png");
+  return (
+    <View className="flex flex-row w-100 justify-between bg-red-300 m-2 mx-6 p-2 rounded-lg">
+      <Image style={{width:50, height:50}} className="w-100 h-100" src={props.picture} source={noImage} />
+      <View className="align-middle">
+        <Text className="text-right text-xl font-extrabold"> {props.title} </Text>
+        <Text className="text-right"> {props.price} + TAX </Text>
+      </View>
+    </View>
+  )
+}
