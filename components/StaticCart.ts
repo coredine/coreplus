ef695cad0e0799239e8e1fca9a023f5d3e147694
JF195 <davidefuoco63@gmail.com>
2025-02-23 10:47:46 -0500

    add static cart class

---
+++ components/StaticCart.ts
@@ -0,0 +1,13 @@
+import { Product } from "./Product";
+
+export class StaticCart {
+    private static product : Product[] = [];
+    
+    public static productList() : Product[]{
+        return this.product;
+    }
+
+    public static addProduct(newProduct : Product){
+        return this.product.push(newProduct)
+    }
+}
