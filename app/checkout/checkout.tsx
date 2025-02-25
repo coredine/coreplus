b2f031ba6f54bb78e19cc10d1df6a06ad1a0720d
Olivier Mansuy <oliviermansuy7@gmail.com>
2025-02-25 06:29:06 -0500

    Single select logic

--- app/checkout/checkout.tsx
+++ app/checkout/checkout.tsx
@@ -10,38 +10,81 @@ export interface CheckoutProperties {
 }
 
 export interface CheckoutStates {
-    smartCartSelected: boolean;
-    paypalSelected: boolean;
-    creditCardSelected: boolean;
-    platformMethodSelected: boolean;
+    options: {
+        smartCartSelected: boolean;
+        paypalSelected: boolean;
+        creditCardSelected: boolean;
+        platformMethodSelected: boolean;
+    };
+    selectedOptionKey: string | undefined;
+    canProceed: boolean;
 }
 
 export default class Checkout extends Component<CheckoutProperties, CheckoutStates, any> {
     constructor(properties: CheckoutProperties) {
         super(properties);
         this.state = {
-            smartCartSelected: false,
-            paypalSelected: false,
-            creditCardSelected: false,
-            platformMethodSelected: false
+            options: {
+                smartCartSelected: false,
+                paypalSelected: false,
+                creditCardSelected: false,
+                platformMethodSelected: false
+            },
+            selectedOptionKey: undefined,
+            canProceed: true
         }
     }
 
     componentDidUpdate(prevProps: Readonly<CheckoutProperties>, prevState: Readonly<CheckoutStates>, snapshot?: any): void {
-        console.log("AAAHHHHH");
+        this.checkSingleSelect();
+        this.updateProceedState();
+    }
+
+    private iterateStateObjectEntries = (object: {}, callback: (key: string, value: any) => void): void => {
+        for (const [key, value] of Object.entries(object)) callback(key, value);
+    }
+
+    private checkSingleSelect = (): void => {
+        this.iterateStateObjectEntries(this.state.options, (key: string, selected: any) => {
+            if (this.state.selectedOptionKey === undefined && selected) this.setState({selectedOptionKey: key});
+            else if (this.state.selectedOptionKey !== key && selected) {
+                this.setState({options: {...this.state.options, [this.state.selectedOptionKey as string]: false}});
+                this.setState({selectedOptionKey: key});
+            }
+        });
+    }
+
+    private updateProceedState = (): void => {
+        // this.iterateStateObjectEntries(this.state.options, (key: string, value: any) => {
+        //     if (value) this.setState({canProceed: true});
+        // });
+        this.setState({canProceed: false});
+    }
+
+    private resetOptions = (): void => {
+        this.iterateStateObjectEntries(this.state.options, (key: string, value: any) => {
+            this.setState({options: {...this.state.options, [key]: false}});
+        });
+    }
+
+    private proceed = () => {
+        // switch (this.state.selectedOptionKey) {
+        //     case "":
+        //         break;
+        //     case "...":
+        //         break;
+        // }
+        console.log("salut")
     }
 
     private getPlatformPaymentMethodButton = (): ReactElement | null => {
         if (Platform.OS == "android") 
-            return <PaymentMethodCard name={"Google Pay"} icons={[faGooglePay]} height={"14%"} value={this.state.platformMethodSelected} onValueChanged={(value: boolean) => {this.setState({platformMethodSelected: value})}} implemented={false} description={undefined}/>;
+            return <PaymentMethodCard name={"Google Pay"} icons={[faGooglePay]} height={"14%"} value={this.state.options.platformMethodSelected} onValueChanged={(value: boolean) => {this.setState({options: {...this.state.options, platformMethodSelected: value}})}} implemented={false} description={undefined}/>;
         else if (Platform.OS == "ios") 
-            return <PaymentMethodCard name={"Apple Pay"} icons={[faApplePay]} height={"14%"} value={this.state.platformMethodSelected} onValueChanged={(value: boolean) => {this.setState({platformMethodSelected: value})}} implemented={false} description={undefined}/>;
+            return <PaymentMethodCard name={"Apple Pay"} icons={[faApplePay]} height={"14%"} value={this.state.options.platformMethodSelected} onValueChanged={(value: boolean) => {this.setState({options: {...this.state.options, platformMethodSelected: value}})}} implemented={false} description={undefined}/>;
         return null;
     }
 
-    private proceed = () => {
-        console.log("oui");
-    }
 
     render(): ReactNode {
         return(
@@ -54,16 +97,32 @@ export default class Checkout extends Component<CheckoutProperties, CheckoutStat
                     <PaymentMethodCard 
                     description={"SmartCart custom paying service."} 
                     name={"SmartCart"} implemented={true} 
-                    onValueChanged={(value: boolean) => {this.setState({smartCartSelected: value})}} 
+                    onValueChanged={(value: boolean) => {this.setState({options: {...this.state.options, smartCartSelected: value}})}} 
                     icons={[faCartShopping]} 
                     height="14%" 
-                    value={this.state.smartCartSelected}/>
-                    <PaymentMethodCard description={undefined} name={"Paypal"} implemented={false} onValueChanged={(value: boolean) => {this.setState({paypalSelected: value})}} icons={[faPaypal]} height="14%" value={this.state.paypalSelected}/>
-                    <PaymentMethodCard description={undefined} name={"Credit Card or Debit Card"} implemented={false} onValueChanged={(value: boolean) => {this.setState({creditCardSelected: value})}} icons={[faCcMastercard, faCcVisa]} height="14%" value={this.state.creditCardSelected}/>
+                    value={this.state.options.smartCartSelected}/>
+
+                    <PaymentMethodCard 
+                    description={undefined} 
+                    name={"Paypal"} 
+                    implemented={false} 
+                    onValueChanged={(value: boolean) => {this.setState({options: {...this.state.options, paypalSelected: value}})}} 
+                    icons={[faPaypal]} 
+                    height="14%" 
+                    value={this.state.options.paypalSelected}/>
+
+                    <PaymentMethodCard
+                    description={undefined} 
+                    name={"Credit Card or Debit Card"} 
+                    implemented={false} 
+                    onValueChanged={(value: boolean) => {this.setState({options: {...this.state.options, creditCardSelected: value}})}} 
+                    icons={[faCcMastercard, faCcVisa]} height="14%" 
+                    value={this.state.options.creditCardSelected}/>
+
                     {this.getPlatformPaymentMethodButton()}
                 </View>
 
-                <CheckoutButtons proceedOnpress={this.proceed} backOnpress={undefined} proceedText={"Continue"} grayedOut={true}/>
+                <CheckoutButtons proceedOnpress={this.proceed} backOnpress={undefined} proceedText={"Continue"} grayedOut={this.state.canProceed}/>
             </View>
         )
     }
--- components/checkoutButtons.tsx
+++ components/checkoutButtons.tsx
@@ -2,6 +2,7 @@ import { Component, ReactNode } from "react";
 import { View, TouchableOpacity, Text } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { AnimatableNumericValue } from "react-native";
 
 export interface CheckoutButtonsProperties {
     backOnpress: (() => void) | undefined;
@@ -23,8 +24,10 @@ export class CheckoutButtons extends Component<CheckoutButtonsProperties, any, a
                         <FontAwesomeIcon icon={faArrowLeft} size={50} style={{margin: "auto"}}/>
                     </TouchableOpacity>
 
-                    <TouchableOpacity className={`bg-blue-500 w-2/5 rounded-full h-3/4 m-2 opacity-${this.props.grayedOut ? 50 : 100}`} onPress={this.props.grayedOut ? undefined : this.props.proceedOnpress}>
-                        <Text className="m-auto font-extrabold">{this.props.proceedText}</Text>
+                    <TouchableOpacity className="bg-blue-500 w-2/5 rounded-full h-3/4 m-2" onPress={this.props.grayedOut ? undefined : this.props.proceedOnpress}>
+                        <View className="m-auto" style={{opacity: 50}}>
+                            <Text className="m-auto font-extrabold">{this.props.proceedText}</Text>
+                        </View>
                     </TouchableOpacity>
                 </View>
             </View>
