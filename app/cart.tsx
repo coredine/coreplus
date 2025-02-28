2fbd809c4a10236ceec339ae1f01316b65052358
JF195 <davidefuoco63@gmail.com>
2025-02-28 13:40:26 -0500

    add wait overlay when scanning an item

--- app/cart.tsx
+++ app/cart.tsx
@@ -1,6 +1,6 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import ResizableComponent from '../components/ResizableComponent';
-import Camera from '../components/camera';
+import Camera, { ScanMode } from '../components/camera';
 import { useRef, useState } from 'react';
 import { BarcodeScanningResult } from 'expo-camera';
 import ProductCard, { Product } from '../components/Product';
@@ -19,26 +19,33 @@ export default function App() {
   StaticCart.setTrigger(trigger, setTrigger)
   
   return (
-    <ResizableComponent childOne={ ( 
-      <View style={styles.container}>
-        <Text style={styles.text}>Scanner</Text> 
-        <Camera barcodeType='code128' width={300} height={180} scanMode={StaticCart.getScanMode()} onBarcodeScanned={async (value: BarcodeScanningResult) => {
-          console.log(value.data)
-          StaticCart.scanOff();
-          await getProductBySKU(instance.current, value.data);
-        }} />
-      </View>
-    ) } childTwo={ ( 
-      <ScrollView className='m-3 bg-gray-200 rounded-lg py-2'>
-        {productList.map( (value : Product, index) => (
-          <ProductCard key={index} picture={value?.picture} 
-            sku={value.sku} title={value.title}
-            price={value.price} weight={value.weight}
-            onPressRemove={() => {console.log("remove " + value.sku)}}
-            />
-        ))}
-      </ScrollView>
-    ) } />
+    <>
+      <ResizableComponent childOne={ ( 
+        <View style={styles.container}>
+          <Text style={styles.text}>Scanner</Text>
+          <Camera barcodeType='code128' width={300} height={180} scanMode={StaticCart.getScanMode()} onBarcodeScanned={async (value: BarcodeScanningResult) => {
+            console.log(value.data);
+            StaticCart.scanOff();
+            await getProductBySKU(instance.current, value.data);
+          } } />
+        </View>
+      ) } childTwo={ ( 
+        <ScrollView className='m-3 bg-gray-200 rounded-lg py-2'>
+          {productList.map( (value : Product, index) => (
+            <ProductCard key={index} picture={value?.picture} 
+              sku={value.sku} title={value.title}
+              price={value.price} weight={value.weight}
+              onPressRemove={() => {console.log("remove " + value.sku)}}
+              />
+          ))}
+        </ScrollView>
+      ) } />
+        {(StaticCart.getScanMode() != ScanMode.NEVER) ? <></> :
+          <View style={{position:'absolute', top:0, bottom:0, left:0, right:0, backgroundColor:"black", opacity:0.2}}>
+
+          </View>
+        }
+    </>
   );
 }
 
