5296d2af341586ef37ebe7f2957afaa68254722a
Olivier Mansuy <oliviermansuy7@gmail.com>
2025-03-03 05:19:00 -0500

    Storage service

--- service/StorageService.ts
+++ service/StorageService.ts
@@ -1,15 +1,28 @@
-import { AsyncStorageStatic } from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default class Storage {
 
     private static key = "customerAuth";
 
-    static async setAuthToken(token: String): Promise<void> {
-        
+    static async setAuthToken(token: string): Promise<void> {
+        try {
+            await AsyncStorage.setItem(this.key, token);
+        } catch (error: unknown) {
+            console.log("SETAUTHTOKEN: " + error)
+        }
     }
 
-    static async getAuthToken(): Promise<String> {
-        return "...";
+    static async getAuthToken(): Promise<string | null> {
+        try {
+            return await AsyncStorage.getItem(this.key);
+        } catch (error: unknown) {
+            console.log("GETAUTHTOKEN: " + error)
+            return null;
+        }
+    }
+
+    static async reset(): Promise<void> {
+        await AsyncStorage.removeItem(this.key);
     }
 
 }
