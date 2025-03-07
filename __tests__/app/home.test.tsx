ebbd2fdc04fa401ea40a70e0ba06c42837e0a5c4
JF195 <davidefuoco63@gmail.com>
2025-03-07 13:07:18 -0500

    update home test so it can work

--- __tests__/app/home.test.tsx
+++ __tests__/app/home.test.tsx
@@ -3,9 +3,14 @@ import Home from '../../app/home';
 
 const MockHomePage = jest.fn(() => <Home />)
 
+jest.mock("../../service/BluetoothService", () => ({
+  getInstance: jest.fn( () => ({scanBackground: jest.fn()})),
+  scanBackground: jest.fn()
+}))
+
 describe("Test home page", () => {
   it("should render the component", () => {
-    let valueToCheck = "Scan QR Code to connect";
+    let valueToCheck = "Scan QR code to connect to a SmartCart.";
 
     const { getByText } = renderRouter({
       "/home": MockHomePage
