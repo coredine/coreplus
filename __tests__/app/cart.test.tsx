98c66f3c9aaf0fcf352b0f2bc2e2446c2842d255
JF195 <davidefuoco63@gmail.com>
2025-03-07 13:33:29 -0500

    add cart test

---
+++ __tests__/app/cart.test.tsx
@@ -0,0 +1,29 @@
+import { renderRouter, screen } from 'expo-router/testing-library';
+import Cart from '../../app/cart';
+import { StaticCart } from '../../components/StaticCart';
+
+const MockCartPage = jest.fn(() => <Cart />)
+
+jest.mock("../../service/BluetoothService", () => ({
+  getInstance: jest.fn( () => ({
+    scanBackground: jest.fn(),
+    isConnected: jest.fn( () => true)
+  }))
+}))
+
+jest.mock('expo-router', () => ({
+  router: { push: jest.fn(), replace: jest.fn() }
+}));
+
+describe("Test home page", () => {
+  it("should render the component", () => {
+    let valueToCheck = "Ready to pay";
+
+    const { getByText } = renderRouter({
+    "/cart": MockCartPage
+    }, {
+    "initialUrl": "/cart"
+    });
+    expect(getByText(valueToCheck)).toBeDefined();
+  })
+})
