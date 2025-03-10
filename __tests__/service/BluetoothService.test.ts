6667bc46909f9f7864e17acc920c2ac99cf7e16a
JF195 <davidefuoco63@gmail.com>
2025-03-09 20:20:13 -0400

    add more tests

--- __tests__/service/BluetoothService.test.ts
+++ __tests__/service/BluetoothService.test.ts
@@ -1,4 +1,6 @@
+import { Characteristic } from "react-native-ble-plx";
 import BluetoothService from "../../service/BluetoothService";
+import { StaticCart } from "../../service/StaticCart";
 
 jest.mock("react-native-ble-plx", ()=> ({
   BleError: jest.fn( () => ({})),
@@ -8,7 +10,8 @@ jest.mock("react-native-ble-plx", ()=> ({
       name:"test", 
       discoverAllServicesAndCharacteristics: jest.fn(),
       monitorCharacteristicForService: jest.fn(),
-      writeCharacteristicWithResponseForService: jest.fn()
+      writeCharacteristicWithResponseForService: jest.fn(),
+      cancelConnection: jest.fn( async () => {})
     }) ),
     startDeviceScan: jest.fn(),
     stopDeviceScan: jest.fn()
@@ -17,6 +20,9 @@ jest.mock("react-native-ble-plx", ()=> ({
 
 const inst = BluetoothService.getInstance();
 describe("Test Bluetooth service", () => {
+  beforeAll( () => {
+    StaticCart.setTrigger(5, (value:number) => (value+1), () =>{})
+  })
   it("should exist", () => {
     expect(inst).toBeDefined();
   })
@@ -28,5 +34,23 @@ describe("Test Bluetooth service", () => {
   })
   it("should stop scan", async () => {
     expect(await inst.stopScan()).toBeUndefined();
+  })
+  it("should connect", async () => {
+    expect(await inst.connectToDevice("12:34:56:78:90:AB")).toBeUndefined();
+    expect(inst.isConnected()).toBeTruthy();
+  })
+  it("should send and receive sku", async () => {
+    await inst.sendSku("123456", "ADD")
+    // inst.skuCallback(null, null)
+    expect(inst.isConnected()).toBeTruthy();
+  })
+  it("should send payment info", async () => {
+    await inst.sendPaymentInfos("email", "password")
+    // inst.skuCallback(null, null)
+    expect(inst.isConnected()).toBeTruthy();
+  })
+  it("should close connection", async () => {
+    await inst.closeConnection()
+    expect(inst.isConnected()).toBeFalsy();
   })
 })
